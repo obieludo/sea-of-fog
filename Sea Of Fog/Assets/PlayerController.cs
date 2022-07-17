e1d524d7790b7822a5729c0a476980f354818b8a
Alec Stewart <stewart.aw99@gmail.com>
2022-07-17 17:14:52 +1000

    Walking and Jumping
    Ability to walk and Jump
    Ground

--- Sea Of Fog/Assets/PlayerController.cs
+++ Sea Of Fog/Assets/PlayerController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Animator anim;
+    public Rigidbody2D rb;
+    public float jumpForce;
+    public float playerSpeed;
+    public Vector2 jumpHeight;
+    public bool isOnGround;
+    public float positionRadius;
+    public LayerMask ground;
+    public Transform playerPos;
+
+
+
+
     void Start()
     {
-        
+        Collider2D[] colliders = transform.GetComponentsInChildren<Collider2D>();
+        for (int i=0; i < colliders.Length; i++)
+        {
+            for(int k=i + 1; k < colliders.Length; k++)
+            {
+                Physics2D.IgnoreCollision(colliders[i], colliders[k]);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                anim.Play("Walk");
+                    rb.AddForce(Vector2.right * playerSpeed);
+            }
+            else
+            {
+                anim.Play("WalkBack");
+                rb.AddForce(Vector2.left * playerSpeed);
+            }
+        }
+        else
+        {
+            anim.Play("Idle");
+        }
+        isOnGround = Physics2D.OverlapCircle(playerPos.position, positionRadius, ground);
+        if(isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jumping");
+            rb.AddForce(Vector2.up * jumpForce);
+        }
     }
 }
